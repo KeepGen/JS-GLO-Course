15a3a2031b8886272dcd349f070871048f8dc2d4
KeepGen <keeper.gen@gmail.com>
2021-10-22 13:01:49 +0300

    Add: New variables for lesson01

---
+++ script.js
@@ -0,0 +1,11 @@
+let money, income, addExpenses, deposit, mission, period;
+
+money = 'money';
+income = 'income';
+addExpenses = 'add expenses';
+deposit = 'deposit';
+mission = 'mission';
+period = 'period';
+
+alert('Hey, you! Yes, you!');
+console.log('Console.Log text');
